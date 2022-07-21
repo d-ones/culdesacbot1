05f96c188ba2dc5c1bd062c28348b8702d2b63fe
Duncan Jones <dunjones@gmail.com>
2022-07-21 19:25:10 -0400

    Changed for Docker Import
    numpy wasn't importing correctly so I went a different route

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 import os
 import tweepy
 import creds
-import pandas as pd
 
 # Variables
 
@@ -14,47 +13,67 @@ posturl = '&zoom=20&maptype=satellite&size=640x640&key='
 baseurl = 'https://maps.googleapis.com/maps/api/staticmap?center='
 api = tweepy.API(auth)
 
-# https://pythonspeed.com/articles/pandas-read-csv-fast/
-# Engine PyArrow give us better performance than default engine in reading
-df = pd.read_csv('culdesaccoords2021.csv', engine="pyarrow")
+# Establishing lists
 
-while True:
+# List of all coordinates
+
+with open('shufflecoords.csv', newline='')as f:
+    coordrows = f.readlines()
+    coordlines = [coordrow.rstrip() for coordrow in coordrows]
+    f.close()
+
+# List of used coordinates
+
+with open('used.txt', 'r') as fd:
+    usedrows = fd.readlines()
+    usedlines = [usedrow.rstrip() for usedrow in usedrows]
+    fd.close()
+
+# Removing used from all coords
+
+for element in usedlines:
+    if element in coordlines:
+        coordlines.remove(element)
+
+# Start of actual tweeting function
+
+for coord in coordlines:
+
     try:
-        # Generate Content
-        random_coord = df.sample(1)  # pick single random sample
-        coord_array = random_coord.\
-            to_string(header=False, index=False, index_names=False).split(" ")
-        coord_str = " , ".join(coord_array)
-        lat: str = coord_array[0]
-        lon: str = coord_array[1][:-2]
-        nom = \
-            f'https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&zoom=11&format=json'
+
+        usedlines.append(str(coord))
+        with open('used.txt', 'w') as out:
+            for line in usedlines:
+                out.write(f'{line}\n')
+
+        #Generate Content
+
+        text = coord.split(',')
+        lat = (text[0])
+        lon = str(text[1])
+        nom = f'https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&zoom=11&format=json'
         locate = requests.get(nom)
         json = locate.json()
         location = json['display_name']
-        url: str = baseurl + \
-            urllib.parse.quote_plus((coord_str)) + posturl + creds.maps_api
+        url = (baseurl + urllib.parse.quote_plus((str(coord))) + posturl + creds.maps_api)
         response = requests.get(url)
-        len_line = len(lat) + len(lon) + 1
-        imagesave = f'tempscreenshot{len_line}.png'
+        imagesave = f'tempscreenshot{len(coordlines)}.png'
         with open(f'{imagesave}', 'wb') as image:
             image.write(response.content)
 
-        # Put in Twitter API stuff
+        # Twitter API stuff
 
-        image_path: str = f'tempscreenshot{len_line}.png'
-        tweet_text: str = f'{location}\n{coord_str}'
+        image_path = f'tempscreenshot{len(coordlines)}.png'
+        tweet_text = f'{location}\n{lat} , {lon}'
         api.update_status_with_media(tweet_text, image_path)
 
         # Delete and sleep
 
-        os.remove(f'tempscreenshot{len_line}.png')
-        df.drop(random_coord.index[0], inplace=True)
-
-        # Overwrite this new data to existing CSV file
-        df.to_csv('culdesaccoords2021.csv', sep=',', index=False, header=False)
+        os.remove(f'tempscreenshot{len(coordlines)}.png')
 
         # Don't delete this :-) !
+
         time.sleep(1800)
-    except ValueError:  # no entry left for randomize
+
+    except ValueError:
         break
