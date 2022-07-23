db6f77f8576e7685c3e3096016940053480ef69d
Duncan Jones <dunjones@gmail.com>
2022-07-23 02:05:12 -0400

    changed for environmental variables

--- creds.py
+++ creds.py
@@ -1,7 +1,6 @@
-# Add your own creds :-)
-
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
-maps_api = ''
+import os
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_secret = os.environ['ACCESS_SECRET']
+maps_api = os.environ['MAPS_API']
